d9692d9162a4e3fe06cbd2c2e098dcc9c650e024
kimelf12 <ericklima816@gmail.com>
2019-10-02 13:43:00 -0300

    Primeiro commit

---
+++ agg.cpp
@@ -0,0 +1,18 @@
+/*
+*Alunos: 
+*Erick Lima Figueiredo - 98898
+*Sávio Mendes Miranda - 
+*Yago Lopes Lamas - 98897
+*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char **argv){
+
+
+
+    
+    return 0;
+}
