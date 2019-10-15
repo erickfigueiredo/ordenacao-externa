9f4d8f99b521e86b1b17987583270bb843e5d63d
kimelf12 <ericklima816@gmail.com>
2019-10-15 15:17:44 -0300

    QuickSort aplicado e funcionando

--- agg.cpp
+++ agg.cpp
@@ -4,103 +4,122 @@
 #include <cstring>
 using namespace std;
 
-struct Informacao{
-	char* ordena;
-	char* media;
+struct Informacao
+{
+	char *ordena;
+	char *media;
 };
 
-int particiona(Informacao *info, int beg, int end, int pivo) {
+int particiona(Informacao *info, int beg, int end, int pivo)
+{
 	char valorPivo[100];
 	strcpy(valorPivo, info[pivo].ordena);
 	//colocamos o pivo temporariamente na ultima posição
-	swap(info[end-1],info[pivo]);
+	swap(info[end - 1], info[pivo]);
 	// ao acharmos um elemento menor do que o pivo, vamos coloca-lo
 	// na posicao "pos"
 	int pos = beg;
-	for(int i = beg; i < end-1; i++) {
-		if(strcmp(info[i].ordena, valorPivo) > 0){
+	for (int i = beg; i < end - 1; i++)
+	{
+		if (strcmp(info[i].ordena, valorPivo) < 0)
+		{
 			swap(info[pos], info[i]);
 			pos++;
 		}
 	}
 	//coloque o pivo depois do ultimo elemento menor que ele
-	swap(info[pos],info[end-1]);
+	swap(info[pos], info[end - 1]);
 	return pos;
 }
 
-void quickSort2(Informacao *info, int beg, int end) {
-	if(beg == end)
+void quickSort2(Informacao *info, int beg, int end)
+{
+	if (beg == end)
 		return;
 	int pos = particiona(info, beg, end, beg);
 	quickSort2(info, beg, pos);
 	quickSort2(info, pos + 1, end);
 }
 
-void quickSort(Informacao *info, int tam) {
+void quickSort(Informacao *info, int tam)
+{
 	quickSort2(info, 0, tam);
 }
 
-int main(int agrc, char **argv){
-
-	int memoria = atoi(argv[2]), cOrd = 0, cMed = 0, k = 0;
-	char * pOrd, *pMed, *token;
+int main(int agrc, char **argv)
+{
+	char *pOrd, *pMed, *token, *elemento;
+	//vetor que guarda o nome maximo do arquivo a ser criado
+	char temp[1000] = {' '};
+	int memoria = atoi(argv[2]), cOrd, cMed, linha, cont;
+	cOrd = cMed = linha = cont = 0;
 	string aux;
-	char* elemento; 
-	//char contArq = 33;
 
 	ifstream fin(argv[1]);
 	getline(fin, aux);
 	char *cab = (char *)aux.c_str();
 
-
 	pOrd = strstr(cab, argv[3]);
 	pMed = strstr(cab, argv[4]);
 
-	for(int i = 0; &cab[i] != pOrd; i++){
-		if(cab[i] == ',')
+	for (int i = 0; &cab[i] != pOrd; i++)
+	{
+		if (cab[i] == ',')
 			cOrd++;
 	}
 
-	for(int i = 0; &cab[i] != pMed; i++){
-		if(cab[i] == ',')
+	for (int i = 0; &cab[i] != pMed; i++)
+	{
+		if (cab[i] == ',')
 			cMed++;
 	}
 
-	while(!fin.eof()){
+	while (!fin.eof())
+	{
 		Informacao *informacao = new Informacao[memoria];
-		
-		for(int i = 0; i < memoria && !fin.eof();i++){
+
+		for (int i = 0; i < memoria && !fin.eof(); i++)
+		{
 			getline(fin, aux);
-			elemento = (char*)aux.c_str();
+			elemento = (char *)aux.c_str();
 
 			token = strtok(elemento, ",");
 
-
-			for(int j=0; token != NULL;j++){
-				if(cOrd == j){
-					k++;
+			for (int j = 0; token != NULL; j++)
+			{
+				if (cOrd == j)
+				{
+					linha++;
 					//informacao[i].ordena = token;
-					informacao[i].ordena = new char[strlen(token)];
+					informacao[i].ordena = new char[strlen(token) + 1];
 					strcpy(informacao[i].ordena, token);
-				}else if(cMed == j){
+				}
+				else if (cMed == j)
+				{
 					//informacao[i].media = token;
-					informacao[i].media = new char[strlen(token)];
+					informacao[i].media = new char[strlen(token) + 1];
 					strcpy(informacao[i].media, token);
 				}
 				token = strtok(NULL, ",");
 			}
 		}
-		
-		quickSort(informacao, k);
-		for(int i=0;i<k;i++){
-			cout<<informacao[i].ordena<<endl;
+
+		quickSort(informacao, linha);
+		sprintf(temp, "buffer%d.txt", cont++);
+		ofstream fout(temp);
+		for (int i = 0; i < linha; i++)
+		{
+			(i == linha-1)? fout << informacao[i].ordena << ',' << informacao[i].media : fout << informacao[i].ordena << ',' << informacao[i].media << endl;
+
 		}
-		for(int i = 0; i < k; i++){
+
+		for (int i = 0; i < linha; i++)
+		{
 			delete[] informacao[i].ordena;
 			delete[] informacao[i].media;
 		}
 		delete[] informacao;
-		k=0;
+		linha = 0;
 	}
 
 	//mergeSort(v, memoria);
