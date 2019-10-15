a994f2ebfccaeca67c248d65f6533bb24fadc8b6
sirsavio <saviomendesmiranda@gmail.com>
2019-10-15 13:51:58 -0300

    falta merge

--- agg.cpp
+++ agg.cpp
@@ -1,90 +1,108 @@
-/*
-*Alunos:
-*Erick Lima Figueiredo - 98898
-*Savio Mendes Miranda - 98886
-*Yago Lopes Lamas - 98897
-*/
-
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
+#include <cstdio>
 #include <cstring>
-
 using namespace std;
 
-bool comparaChar(int inicio, int fim, char v[], char palavra[]);
-int extraiInt();
-int ordenaString();
-void intercalaArquivo();
-void merge();
-void mergeSort();
+struct Informacao{
+	char* ordena;
+	char* media;
+};
 
-
-int main(int argc, char **argv)
-{
-	int memoria = atoi(argv[2]), linhas, numParametros = 1;
-	string cabecalho;
-
-	//Abre o arquivo passado pelo terminal
-	ifstream fin(argv[1]);
-
-	//Copia a primeira linha dele para uma string
-	getline(fin, cabecalho);
-
-	//Cria um vetor de char com o comprimento da string+1 ('\0')
-	char *linhaPtr = new char[cabecalho.length() + 1];
-
-	//Copia o conteudo da string para o vetor
-	strcpy(linhaPtr, cabecalho.c_str());
-
-	//Esse for conta quantas colunas de dados temos (numParametros)
-	for(int i = 0; linhaPtr[i] != '\0'; i++)
-		if(linhaPtr[i] == ',')
-			numParametros++;
-
-	//Ordenação extrena de 2 caminhos
-	fstream arquivo0("arquivo0.txt");
-	fstream arquivo1("arquivo1.txt");
-	fstream arquivo2("arquivo2.txt");
-	fstream arquivo3("arquivo3.txt");
-	
-	//Auxiliar para pegar o tamanho das linhas
-	string *vet = new string[memoria];
-	int *comprimento = new int[memoria];
-	int maior = -1, arquivo = 0;
-
-		//Ve qual a maior linha das que irao para a memoria
-		for(int i = 0; i < memoria; i++)
-		{
-			getline(fin, vet[i]);
-			comprimento[i] = vet[i].length();
-			if(maior < comprimento[i])
-			{
-				maior = comprimento[i];
-			}
+int particiona(Informacao *info, int beg, int end, int pivo) {
+	char valorPivo[100];
+	strcpy(valorPivo, info[pivo].ordena);
+	//colocamos o pivo temporariamente na ultima posiÃ§Ã£o
+	swap(info[end-1],info[pivo]);
+	// ao acharmos um elemento menor do que o pivo, vamos coloca-lo
+	// na posicao "pos"
+	int pos = beg;
+	for(int i = beg; i < end-1; i++) {
+		if(strcmp(info[i].ordena, valorPivo) > 0){
+			swap(info[pos], info[i]);
+			pos++;
 		}
-
-		//Copia os registros para um vetor de char para trabalharmos
-		char **registros = new char*[memoria];
-
-		for(int i = 0; i < memoria; i++)
-		{
-			registros[i] = new char[maior];
-		}
-
-		for(int i = 0; i < memoria; i++)
-		{
-			strcpy(registros[i], (vet[i]).c_str());
-			cout << registros[i] << endl;
-		}
-
-		//Ordena esse vetor registros
-
-		//Salva ele no primeiro arquivo (0)
-
-		//Salva o vetor ordenado no dispositivo e passa para o proximo
-
-	return 0;
+	}
+	//coloque o pivo depois do ultimo elemento menor que ele
+	swap(info[pos],info[end-1]);
+	return pos;
 }
 
+void quickSort2(Informacao *info, int beg, int end) {
+	if(beg == end)
+		return;
+	int pos = particiona(info, beg, end, beg);
+	quickSort2(info, beg, pos);
+	quickSort2(info, pos + 1, end);
+}
 
+void quickSort(Informacao *info, int tam) {
+	quickSort2(info, 0, tam);
+}
+
+int main(int agrc, char **argv){
+
+	int memoria = atoi(argv[2]), cOrd = 0, cMed = 0, k = 0;
+	char * pOrd, *pMed, *token;
+	string aux;
+	char* elemento; 
+	//char contArq = 33;
+
+	ifstream fin(argv[1]);
+	getline(fin, aux);
+	char *cab = (char *)aux.c_str();
+
+
+	pOrd = strstr(cab, argv[3]);
+	pMed = strstr(cab, argv[4]);
+
+	for(int i = 0; &cab[i] != pOrd; i++){
+		if(cab[i] == ',')
+			cOrd++;
+	}
+
+	for(int i = 0; &cab[i] != pMed; i++){
+		if(cab[i] == ',')
+			cMed++;
+	}
+
+	while(!fin.eof()){
+		Informacao *informacao = new Informacao[memoria];
+		
+		for(int i = 0; i < memoria && !fin.eof();i++){
+			getline(fin, aux);
+			elemento = (char*)aux.c_str();
+
+			token = strtok(elemento, ",");
+
+
+			for(int j=0; token != NULL;j++){
+				if(cOrd == j){
+					k++;
+					//informacao[i].ordena = token;
+					informacao[i].ordena = new char[strlen(token)];
+					strcpy(informacao[i].ordena, token);
+				}else if(cMed == j){
+					//informacao[i].media = token;
+					informacao[i].media = new char[strlen(token)];
+					strcpy(informacao[i].media, token);
+				}
+				token = strtok(NULL, ",");
+			}
+		}
+		
+		quickSort(informacao, k);
+		for(int i=0;i<k;i++){
+			cout<<informacao[i].ordena<<endl;
+		}
+		for(int i = 0; i < k; i++){
+			delete[] informacao[i].ordena;
+			delete[] informacao[i].media;
+		}
+		delete[] informacao;
+		k=0;
+	}
+
+	//mergeSort(v, memoria);
+	return 0;
+}
